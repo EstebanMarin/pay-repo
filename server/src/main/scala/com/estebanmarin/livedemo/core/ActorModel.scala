8edcbc250c6301ebca51fd505475ed2fd1bae31c
EstebanMarin <estebanmarin002@gmail.com>
2024-07-05 20:51:02 -0500

    feat: Update ActorModel to use SimpleActorBehavior_v3
    This commit updates the ActorModel class in ActorModel.scala to use the new SimpleActorBehavior_v3 instead of the previous SimpleActorBehavior. The change improves logging of received messages and enhances the overall behavior of the actor system.

--- server/src/main/scala/com/estebanmarin/livedemo/core/ActorModel.scala
+++ server/src/main/scala/com/estebanmarin/livedemo/core/ActorModel.scala
@@ -3,17 +3,17 @@ package com.estebanmarin.livedemo.core
 import cats.effect.*
 import cats.syntax.all.*
 import cats.implicits.*
-import cats.Applicative
+import cats.*
 import org.apache.pekko.actor.typed.{Behavior, ActorSystem}
 import org.apache.pekko.actor.typed.scaladsl.Behaviors
 import org.checkerframework.checker.units.qual.m
 
 trait ActorModel[F[_]] {
   def useActorModel: F[String]
-  def demoSimpleActor: F[Unit]
+  def demoSimpleActor(msg: String): F[Unit]
 }
 
-class ActorModelLive[F[_]: Applicative] extends ActorModel[F] {
+class ActorModelLive[F[_]: Monad] extends ActorModel[F] {
   // part 1 define a behevior of an actor
   val simpleActorBehavior: Behavior[String] = Behaviors.receiveMessage((msg: String) => {
     println(s"Received message: $msg")
@@ -49,10 +49,10 @@ class ActorModelLive[F[_]: Applicative] extends ActorModel[F] {
   // val simpleActor: ActorSystem[String] = ActorSystem(SimpleActorBehavior_v3(), "simpleActor")
   val simpleActor: ActorSystem[String] = ActorSystem(Person.happy(), "simpleActor")
 
-  def demoSimpleActor: F[Unit] = {
+  def demoSimpleActor(msg: String): F[Unit] = {
     // part 2 instanciate an actor system
     // part 3 send a message to the actor
-    (simpleActor ! "Hello, World!").pure[F]
+    (simpleActor ! msg).pure[F]
   }
 
   // exercise
@@ -73,6 +73,7 @@ class ActorModelLive[F[_]: Applicative] extends ActorModel[F] {
 }
 
 object ActorModelLive {
-  def make[F[_]: Applicative]: F[ActorModel[F]]               = new ActorModelLive[F].pure[F]
-  def resource[F[_]: Applicative]: Resource[F, ActorModel[F]] = Resource.pure(new ActorModelLive[F])
+  def make[F[_]: Monad]: F[ActorModel[F]] = new ActorModelLive[F].pure[F]
+  def resource[F[_]: Monad]: Resource[F, ActorModel[F]] =
+    Resource.pure(new ActorModelLive[F])
 }
--- server/src/main/scala/com/estebanmarin/livedemo/http/ActorModelRoutes.scala
+++ server/src/main/scala/com/estebanmarin/livedemo/http/ActorModelRoutes.scala
@@ -12,15 +12,36 @@ import io.circe.generic.auto.*
 import com.estebanmarin.livedemo.core.*
 import com.estebanmarin.livedemo.domain.job.*
 import org.http4s.server.Router
+import com.stripe.param.treasury.InboundTransferFailParams.FailureDetails.Code
+import io.circe.Codec
 
 class ActorModelRoutes[F[_]: Concurrent] private (actorM: ActorModel[F]) extends Http4sDsl[F] {
   private val prefix = "/actorModel"
 
+  case class MessageRequest(message: String) derives Codec.AsObject
+
   private val demoSimpleActorRoute: HttpRoutes[F] = HttpRoutes.of[F] { case _ @GET -> Root =>
     for {
-      _        <- actorM.demoSimpleActor
+      _        <- actorM.demoSimpleActor("actorModel")
       response <- Ok("actorModel")
     } yield response
+
+//     ❯ http POST "http://localhost:4041/actorModel" message="Hello Esteban"
+// HTTP/1.1 200 OK
+// Connection: keep-alive
+// Content-Length: 33
+// Content-Type: application/json
+// Date: Sat, 06 Jul 2024 01:40:01 GMT
+
+// "Received message: Hello Esteban"
+// INFO com.estebanmarin.livedemo.core.ActorModelLive - Happy: Hello Esteban
+
+    case req @ POST -> Root =>
+      for {
+        messageReq <- req.as[MessageRequest] // Decode the request body into MessageRequest
+        _          <- actorM.demoSimpleActor(messageReq.message)
+        response   <- Ok(s"Received message: ${messageReq.message}")
+      } yield response
   }
 
   val routes: HttpRoutes[F] = Router(
